7d3df9c79bba44f7c9756404e980f037d94ca926
Wato-Adano <watobuchaadano@gmail.com>
2024-02-07 08:44:08 +0300

    add a file

---
+++ Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val studentName = "wato adano"
+    println(studentName)
+    var age = 20
+    println(age)
+    var phoneNumber = 254768640065
+    println(phoneNumber)
+    var weight = 54
+    println(weight)
+    var nationality = "kenya"
+    println(nationality)
+    val citizen =true
+    val nonCitizen = false
+    println(citizen)
+
+
+}
